375291a832bdddb955856929cecaf157f89e74cd
Lucas <lucas.loliveira@hotmail.com>
2015-01-18 19:51:21 -0200

    Implemented more less features...

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -14,14 +14,49 @@
   function Home() {
 
     this.tabs = [
-      {title: "Indications Received"},
-      {title: "My Indications"}
+      {title: "Indications Received", type: 'received'},
+      {title: "My Indications", type: 'sent'}
     ];
 
     this.abaSelecionada = 'opa!';
 
+    this.groupedIndicationsSent = [
+      {
+        date: 'Saturday 8th June',
+        indications: [
+          {
+            title: 'Font Awesome Sent',
+            pictureUrl: '/images/yeoman.png',
+            short: 'http://fortawesome.github.io/',
+            url: 'yepin.es/13NfIj1',
+            description: 'The complete set of 479 icons in Font Awesome 4.2.0'
+          }
+        ]
+      }
+    ];
+
+    this.groupedIndicationsReceived = [
+      {
+        date: 'Saturday 8th June',
+        indications: [
+          {
+            title: 'Fonts Received',
+            pictureUrl: '/images/yeoman.png',
+            short: 'http://fortawesome.github.io/',
+            url: 'yepin.es/13NfIj1',
+            description: 'The complete set of 479 icons in Font Awesome 4.2.0'
+          }
+        ]
+      }
+    ];
+
     this.changeTab = function(tab) {
       this.abaSelecionada = tab.title;
+      if (tab.type === 'received') {
+        this.groupedIndications = this.groupedIndicationsReceived;
+      } else {
+        this.groupedIndications = this.groupedIndicationsSent;
+      }
     };
   }
 })();
