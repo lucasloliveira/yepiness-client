6502b5dd7a7de719623727ca26c0c44cb409b356
Lucas Oliveira <lucas.loliveira@hotmail.com>
2015-02-12 23:31:03 -0200

    change * to localhost

--- Gruntfile.js
+++ Gruntfile.js
@@ -104,7 +104,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '*',
+        hostname: 'localhost',
         changeOrigin: true,
         livereload: 35729
       },
